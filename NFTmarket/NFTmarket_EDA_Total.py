b8440515d5af9613feccd80f98c00d3c53f5c73c
ggmaeng@gmail.com <ggmaeng@gmail.com>
2022-03-21 08:52:12 +0000

    #45 시계열 검증, arima, prophet test

--- NFTmarket/NFTmarket_EDA_Total.py
+++ NFTmarket/NFTmarket_EDA_Total.py
@@ -1457,11 +1457,440 @@ heatmapF(totalM_median_log, 'average_usd')
 
 # COMMAND ----------
 
+# avg usd피처 카테고리 전부다 분석해보자
 
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # 8. 시계열 특성 분석
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 데이터 분리
+
+# COMMAND ----------
+
+series = total['all_average_usd']
+train = total.loc[:'2022-1','all_average_usd']
+test = total.loc['2022-2','all_average_usd']
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 정상화 : 자기 상관 함수(ACF, PACF)
+# MAGIC - 잔차들이 시간의 흐름에서 독립적인지를 확인하기 위함(acf에 0에 가까우면 독립적)
+# MAGIC - ACF는 보통 시계열에서 과거의 종속변수(Y)와의 비교를 통해 계절성 판단을 주로 한다.
+# MAGIC - 보통 시계열 분석에서 많이 사용이 되며, 현재의 Y값과 과거의 Y값의 상관성을 비교한다. 왜냐하면, 각각의 Y값이 독립적이어야 결과 분석이 더 잘되기 때문이다.(Y를 정상화시키면 분석이 더 잘된다는 개념과 같다.) 
+# MAGIC - AR(p) 특성: ACF는 천천히 감소하고, PACF는 처음 시차를 제외하고 급격히 감소
+# MAGIC - MA(q) 특성: ACF가 급격히 감소하고, ACF는 천천히 감소
+# MAGIC - 각각 급격히 감소하는 시차를 모수로 사용한다. AR -> PACF,    MA -> ACF
+
+# COMMAND ----------
+
+from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, acf, pacf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from warnings import filterwarnings
+filterwarnings("ignore")
+plt.style.use("ggplot")
+
+# COMMAND ----------
+
+def autoCorrelation_stack(series):
+    acf_array = acf(series.dropna(), alpha=0.05) 
+    pacf_array = pacf(series.dropna(), alpha=0.05)
+    
+    array_list = [acf_array, pacf_array]
+    for i in range(len(array_list)) :
+        corr_array = array_list[i]
+        lower_y = corr_array[1][:,0] - corr_array[0]
+        upper_y = corr_array[1][:,1] - corr_array[0]
+
+        fig = go.Figure()
+        [fig.add_scatter(x=(x,x), y=(0,corr_array[0][x]), mode='lines',line_color='#3f3f3f') 
+         for x in range(len(corr_array[0]))]
+        fig.add_scatter(x=np.arange(len(corr_array[0])), y=corr_array[0], mode='markers', marker_color='#1f77b4',
+                       marker_size=12)
+        fig.add_scatter(x=np.arange(len(corr_array[0])), y=upper_y, mode='lines', line_color='rgba(255,255,255,0)')
+        fig.add_scatter(x=np.arange(len(corr_array[0])), y=lower_y, mode='lines',fillcolor='rgba(32, 146, 230,0.3)',
+                fill='tonexty', line_color='rgba(255,255,255,0)')
+        fig.update_traces(showlegend=False)
+        fig.update_xaxes(range=[-1,42])
+        fig.update_yaxes(zerolinecolor='#000000')
+        
+        title= 'Autocorrelation (ACF)' if i == 0 else 'Partial Autocorrelation (PACF)' 
+        fig.update_layout(title=title)
+        fig.show()
+
+# COMMAND ----------
+
+# ACF는 천천히 감소하고 PACF가 급격히 감소하므로 AR의 특성을 가짐
+# pacf는 time lag 1에서 0.95을 보이고 3까지 급격히 감소, q = 3-1 = 2
+
+autoCorrelation(series)
+
+# COMMAND ----------
+
+# 차분 함수 , 차분 그래프 함수, adfuller 함수
+# aic-> arima 함수
+# 데이터프레임으로 기록?
+# prophet 스터디
+
+
+# COMMAND ----------
+
+def autoCorrelationF(data, feature):
+    
+        # 피처 분류기 호출
+    col_list = feature_classifier(data, feature)
+    
+    for col in col_list:
+        series = data[col]
+
+        acf_array = acf(series.dropna(), alpha=0.05) 
+        pacf_array = pacf(series.dropna(), alpha=0.05)
+
+        array_list = [acf_array, pacf_array]
+
+        fig = make_subplots(rows=1, cols=2)
+
+        for i in range(len(array_list)) :
+            corr_array = array_list[i]
+            lower_y = corr_array[1][:,0] - corr_array[0]
+            upper_y = corr_array[1][:,1] - corr_array[0]
+
+            [fig.add_scatter(x=(x,x), y=(0,corr_array[0][x]), mode='lines',line_color='#3f3f3f', row=1, col=i+1)
+             for x in range(len(corr_array[0]))]
+
+
+            fig.add_scatter(x=np.arange(len(corr_array[0])), y=corr_array[0], mode='markers', marker_color='#1f77b4', marker_size=12, row=1, col=i+1)
+
+            fig.add_scatter(x=np.arange(len(corr_array[0])), y=upper_y, mode='lines', line_color='rgba(255,255,255,0)', row=1, col=i+1)
+
+            fig.add_scatter(x=np.arange(len(corr_array[0])), y=lower_y, mode='lines',fillcolor='rgba(32, 146, 230,0.3)',
+                fill='tonexty', line_color='rgba(255,255,255,0)', row=1, col=i+1)
+
+
+            fig.update_traces(showlegend=False)
+            fig.update_xaxes(range=[-1,42])
+            fig.update_yaxes(zerolinecolor='#000000')
+
+        fig.update_layout(title= f'<b>[{col}] Autocorrelation (ACF)                                 [{col}] Partial Autocorrelation (PACF)<b>', 
+                         title_x=0.5)
+        fig.show()
+
+# COMMAND ----------
+
+autoCorrelationF(total, 'average_usd') #raw df, feature
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 차분하여 다시 체크
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+def differ(data) :
+    
+
+# COMMAND ----------
+
+# 21년 11월 15~16일 급등락이 매우 큼, 그외에는 정상성을 보이므로 1차 차분으로 충분
+# arima(0,1,2)하면 될듯
+diff_series = series.diff(periods=1).dropna() # dropna()는 diff를 통해 생긴 데이터 공백제거
+px.line(diff_series)     
+
+# COMMAND ----------
+
+autoCorrelation(diff_series)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 시계열 검증(adfuller)
+# MAGIC - 검증 조건 ( p-value : 5%이내면 reject으로 대체가설 선택됨 )
+# MAGIC - 귀무가설(H0): non-stationary.
+# MAGIC - 대체가설 (H1): stationary.
+
+# COMMAND ----------
+
+from statsmodels.tsa.stattools import adfuller
+
+# COMMAND ----------
+
+# raw데이터 : p-value가 0.05보다 크므로 귀무가설 채택하여 "비정상성"
+print(adfuller(series)[0]) # adf 작을 수록 귀무가설을 기각시킬 확률이 높다
+print(adfuller(series)[1]) # p-value
+
+# COMMAND ----------
+
+# raw데이터 : p-value가 0.05보다 작으므로 귀무가설 기각하여 "정상성"
+print(adfuller(diff_series)[0]) # adf 작을 수록 귀무가설을 기각시킬 확률이 높다
+print(adfuller(diff_series)[1]) # p-value
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # 모델링
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## ARIMA
+
+# COMMAND ----------
+
+order = (0, 1, 2)
+model = ARIMA(train, order)
+rfit = model.fit()
+rfit.summary()
+
+# COMMAND ----------
+
+# (0,1,1)이 정말 최선일지, 모든 경우의 수의 aic값을 체크해보자
+def arima_aic_check(data, order,sort = 'AIC'):
+    order_list = []
+    aic_list = []
+    bic_lsit = []
+    for p in range(order[0]):
+        for d in range(order[1]):
+            for q in range(order[2]):
+                model = ARIMA(data, order=(p,d,q))
+                try:
+                    model_fit = model.fit()
+                    c_order = f'p:{p} d:{d} q:{q}'
+                    aic = model_fit.aic
+                    bic = model_fit.bic
+                    order_list.append(c_order)
+                    aic_list.append(aic)
+                    bic_list.append(bic)
+                except:
+                    pass
+    result_df = pd.DataFrame(list(zip(order_list, aic_list)),columns=['order','AIC'])
+    result_df.sort_values(sort, inplace=True)
+    return result_df
+
+# COMMAND ----------
+
+# aic값이 가장 작은 모델을 선택, 12번 (1,1,2)의 aic값이 가장 작다
+arima_aic_check(train, [3,3,3])
+
+# COMMAND ----------
+
+# P>z값이 일반적으로 학습의 적정성을 위해 호가인되는 T-검정값
+# 즉, p-value 0.05수준에서 보면 MA(1)와 MA(2)의 값은 유효한데, 모형의 constant는 유효하지 않다. 따라서 모형의 model.fit()파라미터중trend='c'가 아니라 nc로 설정하는 것이 옳다
+order = (1, 1, 2)
+model = ARIMA(train, order)
+rfit = model.fit()
+rfit.summary()
+
+# COMMAND ----------
+
+# constraint가 없는 모형으로 fitting하니 t 검정값이 더 좋아짐?
+order = (1, 1, 2)
+model = ARIMA(train, order)
+model_fit = model.fit(trend='nc')
+model_fit.summary()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## ARIMA-예측
+
+# COMMAND ----------
+
+model_fit.plot_predict()
+
+# COMMAND ----------
+
+# model_fit.plot_predict('2017-06-23', '2022-02-28')
+model_fit.plot_predict(1, 1720)
+
+# COMMAND ----------
+
+fore = model_fit.forecast(steps=1)
+print(fore)
+# 예측값, stderr, upperbound, lower bound
+# 2월 1일을 573.9라고 예측, 비슷한 듯
+
+# COMMAND ----------
+
+series['2022-02-01']
+
+# COMMAND ----------
+
+# 과거 데이터로 테스트해보자
+model_fit.predict(1, 10, typ='levels') # typ= default값이 linear, 예측할때 levels
+
+# COMMAND ----------
+
+# ... 뭥미?
+train[1:10]
+
+# COMMAND ----------
+
+# 미래 예측하기 # 왜 안되지 ㅜㅜ
+preds = model_fit.predict('2022-02-01', '2022-02-28', typ='levels')
+preds
+
+# COMMAND ----------
+
+preds = model_fit.predict(1,1720, typ='levels')
+preds
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Prophet
+# MAGIC - https://facebook.github.io/prophet/docs/quick_start.html#python-api
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## quick start
+
+# COMMAND ----------
+
+!pip install Prophet
+
+# COMMAND ----------
+
+import pandas as pd
+from prophet import Prophet
+
+
+# COMMAND ----------
+
+# 칼럼명 변경
+df = series.reset_index()
+df.columns = ['ds', 'y']
+print(df)
+
+
+# COMMAND ----------
+
+m = Prophet()
+m.fit(df)
+
+# COMMAND ----------
+
+future = m.make_future_dataframe(periods=14)
+future.tail()
+
+# COMMAND ----------
+
+# yhat : 응답변수의 추정값
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+# COMMAND ----------
+
+from prophet.plot import plot_plotly, plot_components_plotly
+
+plot_plotly(m, forecast)
+
+# COMMAND ----------
+
+1# Python
+plot_components_plotly(m, forecast)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Forecasting Growth
+
+# COMMAND ----------
+
+df['cap'] = 8.5
+
+# COMMAND ----------
+
+mlg = Prophet(growth='logistic')
+mln = Prophet(growth='linear')
+mlg.fit(df)
+mln.fit(df)
+
+
+# COMMAND ----------
+
+# 로지스틱 예측
+future = mlg.make_future_dataframe(periods=365)
+future['cap'] = 8.5
+fcst = mlg.predict(future)
+fig = mlg.plot(fcst)
+
+# COMMAND ----------
+
+# 선형 예측
+future = mln.make_future_dataframe(periods=365)
+future['cap'] = 8.5
+fcst = mln.predict(future)
+fig = mln.plot(fcst)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Saturating Minimum
+
+# COMMAND ----------
+
+df['y'] = 10 - df['y']
+df['cap'] = 6
+df['floor'] = 1.5
+future['cap'] = 6
+future['floor'] = 1.5
+m = Prophet(growth='logistic')
+m.fit(df)
+fcst = m.predict(future)
+fig = m.plot(fcst)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Trend changepoints
+
+# COMMAND ----------
+
+from prophet.plot import add_changepoints_to_plot
+fig = m.plot(forecast)
+a = add_changepoints_to_plot(fig.gca(), m, forecast)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Adjusting trend flexibility
+
+# COMMAND ----------
+
+m = Prophet(changepoint_prior_scale=0.5)
+forecast = m.fit(df).predict(future)
+fig = m.plot(forecast)
+
+# COMMAND ----------
+
+# 값을 줄여보자
+m = Prophet(changepoint_prior_scale=0.001)
+forecast = m.fit(df).predict(future)
+fig = m.plot(forecast)
+
+# COMMAND ----------
+
+# 변경점 직접 지정
+# Python
+m = Prophet(changepoints=['2020-11-01'])
+forecast = m.fit(df).predict(future)
+fig = m.plot(forecast)
 
 # COMMAND ----------
 
